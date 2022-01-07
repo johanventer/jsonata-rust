76ed468f1a20982c138e0f6c971b7d16b0d16513
Johan Venter <johan@saasquatch.com>
2022-01-06 18:40:23 -0800

    Reorganize for proc macro

--- build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-changed=tests/testsuite/groups/*/*.json");
-    println!("cargo:rerun-if-changed=tests/testsuite/groups");
-}
---
+++ jsonata-signature-macro/src/lib.rs
@@ -0,0 +1,8 @@
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn signature(attr: TokenStream, item: TokenStream) -> TokenStream {
+    println!("attr: \"{}\"", attr.to_string());
+    println!("item: \"{}\"", item.to_string());
+    item
+}
